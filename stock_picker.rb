152eaadb2f059fd1e9345af379c00f6ac31b09b0
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-04-07 15:53:31 -0400

    Add indentation

--- stock_picker.rb
+++ stock_picker.rb
@@ -6,16 +6,16 @@ def stock_picker(prices)
     max_profit = 0 
 
     prices.each_with_index do |buy_price, buy_day|
-    auxiliar.shift
-    auxiliar.each_with_index do |sell_price, sell_day|
+        auxiliar.shift
+        auxiliar.each_with_index do |sell_price, sell_day|
 
-        if (sell_price - buy_price) > max_profit
-        max_profit = sell_price - buy_price
-        best_time_to_buy = buy_day
-        best_time_to_sell = buy_day + sell_day + 1
-        
+            if (sell_price - buy_price) > max_profit
+            max_profit = sell_price - buy_price
+            best_time_to_buy = buy_day
+            best_time_to_sell = buy_day + sell_day + 1
+            
+            end
         end
-    end
     end
 
     return  [best_time_to_buy,best_time_to_sell]
