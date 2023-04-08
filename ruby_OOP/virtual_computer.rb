d09cc28e2bd149c61b2776f0b80eec28057a638b
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-04-08 10:14:05 -0400

    Add users class variable

--- ruby_OOP/virtual_computer.rb
+++ ruby_OOP/virtual_computer.rb
@@ -1,7 +1,9 @@
 class Computer
+    @@users = {}
     def initialize(username, password)
         @username = username
         @password = password
+        @@users[username] = password
         @files = {}
     end
 end
